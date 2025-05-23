d185b48cb7e0e1b8f3fdbe262d70db62ae801ee0
ABigJuicyBurger <bader_muhssin@hotmail.com>
2025-05-23 11:42:22 -0600

    new projec

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,33 +1,42 @@
-import React from 'react';
-import styles from '../styles/ProjectsSection.module.css';
-import Moon3D from './Moon3D';
-import About from './About';
-import SkillsLogos from './SkillsLogos';
+import React from "react";
+import styles from "../styles/ProjectsSection.module.css";
+import Moon3D from "./Moon3D";
+import About from "./About";
+import SkillsLogos from "./SkillsLogos";
 
 const projects = [
   {
-    name: 'JobCompass',
-    description: 'A smart job search platform integrating features similar to Rent Faster and Indeed.',
-    link: 'http://jobcompass.duckdns.org/',
-    tags: ['React', 'Node.js', 'TypeScript'],
+    name: "JobCompass",
+    description:
+      "A smart job search platform integrating features similar to Rent Faster and Indeed.",
+    link: "http://jobcompass.duckdns.org/",
+    tags: ["React", "Node.js", "TypeScript"],
   },
   {
-    name: 'Admin Dashboard',
-    description: 'Dashboard of all projects I\'ve completed through The Odin Project.',
-    link: 'https://abigjuicyburger.github.io/Admin-Dashboard',
-    tags: ['JavaScript', 'APIs', 'Canvas'],
+    name: "Admin Dashboard",
+    description:
+      "Dashboard of all projects I've completed through The Odin Project.",
+    link: "https://abigjuicyburger.github.io/Admin-Dashboard",
+    tags: ["JavaScript", "APIs", "Canvas"],
   },
   {
-    name: 'Shop.AI',
-    description: 'A not so smart e-commerce platform.',
-    link: 'https://shopai-react.netlify.app/',
-    tags: ['React', 'TypeScript', 'JavaScript'],
+    name: "Shop.AI",
+    description: "A not so smart e-commerce platform.",
+    link: "https://shopai-react.netlify.app/",
+    tags: ["React", "TypeScript", "JavaScript"],
   },
   {
-    name: 'Restaurant Page',
-    description: 'A restaurant landing page, flexing my Webpack and npm skills.',
-    link: 'https://abigjuicyburger.github.io/RestaurantPage',
-    tags: ['JavaScript', 'Webpack', 'npm'],
+    name: "Restaurant Page",
+    description:
+      "A restaurant landing page, flexing my Webpack and npm skills.",
+    link: "https://abigjuicyburger.github.io/RestaurantPage",
+    tags: ["JavaScript", "Webpack", "npm"],
+  },
+  {
+    name: "AI vs Jobs",
+    description: `Used Python, to model AI adoption vs. U.S. unemployment, showing AI's role in creating specialized tech jobs.`,
+    link: "https://abigjuicyburger.github.io/data-science-project-clean/ai_unemployment_visualization.html",
+    tags: ["Python", "Scikit", "Pandas"],
   },
 ];
 
